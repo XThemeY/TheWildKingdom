a07c7d28a589e88094bca8e8ced344c42dcef411
XThemeY <xthemey@yandex.ru>
2023-12-06 20:15:46 +0300

    swiper added

--- src/js/main.js
+++ src/js/main.js
@@ -1,85 +1,105 @@
+document.addEventListener("DOMContentLoaded", ready);
 import * as functions from "./modules/functions.js";
-functions.isWebp();
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
 
-const sliderLine = document.querySelector(".head__slider-line");
-const btnRight = document.querySelector(".btn-arrow");
-const slides = document.querySelectorAll(".head-slide");
+function ready() {
+  functions.isWebp();
 
-for (let i = 0; i < slides.length; i++) {
-  sliderLine.insertAdjacentHTML("beforeend", slides[i].outerHTML);
-}
+  const sliderLine = document.querySelector(".head__slider-line");
+  const btnRight = document.querySelector(".btn-arrow");
+  const slides = document.querySelectorAll(".head-slide");
 
-let sliderItems = document.querySelectorAll(".head-slide");
-sliderItems[0].classList.add("head-slide--active");
-let index = 0;
-let offset = 160;
-let lastClick = 0;
-const getSlides = () => document.querySelectorAll(".head-slide");
+  for (let i = 0; i < slides.length; i++) {
+    sliderLine.insertAdjacentHTML("beforeend", slides[i].outerHTML);
+  }
 
-function nextSlide() {
-  document.documentElement.style.setProperty(
-    "--transition",
-    "all ease-in-out 0.5s"
-  );
-  window.innerWidth <= 1130 ? (offset = 130) : (offset = 160);
+  let sliderItems = document.querySelectorAll(".head-slide");
+  sliderItems[0].classList.add("head-slide--active");
+  let index = 0;
+  let offset = 160;
+  let lastClick = 0;
+  const getSlides = () => document.querySelectorAll(".head-slide");
 
-  //Проверка на частые клики мыши
-  if (new Date().getTime() - lastClick < 500) return;
-  lastClick = new Date().getTime();
+  function nextSlide() {
+    document.documentElement.style.setProperty(
+      "--transition",
+      "all ease-in-out 0.5s"
+    );
+    window.innerWidth <= 1130 ? (offset = 130) : (offset = 160);
 
-  sliderItems = getSlides();
+    //Проверка на частые клики мыши
+    if (new Date().getTime() - lastClick < 500) return;
+    lastClick = new Date().getTime();
 
-  if (index >= sliderItems.length - 1) {
-    index = 0;
+    sliderItems = getSlides();
+
+    if (index >= sliderItems.length - 1) {
+      index = 0;
+      sliderLine.style.right = `${offset * index}px`;
+      sliderItems[index].classList.add("head-slide--active");
+      sliderItems[sliderItems.length - 1].classList.remove(
+        "head-slide--active"
+      );
+      return;
+    }
+    sliderItems[index].classList.remove("head-slide--active");
+    index++;
     sliderLine.style.right = `${offset * index}px`;
     sliderItems[index].classList.add("head-slide--active");
-    sliderItems[sliderItems.length - 1].classList.remove("head-slide--active");
-    return;
+    resetSlider();
   }
-  sliderItems[index].classList.remove("head-slide--active");
-  index++;
-  sliderLine.style.right = `${offset * index}px`;
-  sliderItems[index].classList.add("head-slide--active");
-  resetSlider();
-}
-function resetSlider() {
-  if (index == sliderItems.length / 2) {
-    setTimeout(() => {
-      document.documentElement.style.setProperty("--transition", "0s");
-      sliderLine.style.right = "0px";
-      sliderItems[index].classList.remove("head-slide--active");
-      index = 0;
-      sliderItems[index].classList.add("head-slide--active");
-    }, 500);
+  function resetSlider() {
+    if (index == sliderItems.length / 2) {
+      setTimeout(() => {
+        document.documentElement.style.setProperty("--transition", "0s");
+        sliderLine.style.right = "0px";
+        sliderItems[index].classList.remove("head-slide--active");
+        index = 0;
+        sliderItems[index].classList.add("head-slide--active");
+      }, 500);
+    }
   }
-}
-btnRight.addEventListener("click", nextSlide);
-//-----------
-const menuBtn = document.querySelector(".menu__button");
-const menu = document.querySelector(".header__menu");
-const nav = document.querySelector(".menu__body");
-const body = document.body;
+  btnRight.addEventListener("click", nextSlide);
+  //-----------
+  const menuBtn = document.querySelector(".menu__button");
+  const menu = document.querySelector(".header__menu");
+  const nav = document.querySelector(".menu__body");
+  const body = document.body;
 
-document
-  .querySelectorAll(".menu__body .link, .menu__body .button")
-  .forEach((el) => {
-    el.addEventListener("click", showMenu);
+  document
+    .querySelectorAll(".menu__body .link, .menu__body .button")
+    .forEach((el) => {
+      el.addEventListener("click", showMenu);
+    });
+
+  function showMenu(e) {
+    e.stopPropagation();
+    menu.classList.toggle("menu--active");
+    nav.classList.toggle("menu__body--active");
+    body.classList.toggle("body--overflow");
+  }
+
+  nav.addEventListener("click", (e) => {
+    showMenu(e);
   });
 
-function showMenu(e) {
-  e.stopPropagation();
-  menu.classList.toggle("menu--active");
-  nav.classList.toggle("menu__body--active");
-  body.classList.toggle("body--overflow");
+  window.addEventListener("resize", function () {
+    menu.classList.remove("menu--active");
+    nav.classList.remove("menu__body--active");
+    body.classList.remove("body--overflow");
+  });
+  menuBtn.addEventListener("click", showMenu);
+
+  const swiper = new Swiper(".swiper", {
+    modules: [Navigation],
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews__button-next",
+      prevEl: ".reviews__button-prev",
+    },
+  });
+
+  swiper();
 }
-
-nav.addEventListener("click", (e) => {
-  showMenu(e);
-});
-
-window.addEventListener("resize", function () {
-  menu.classList.remove("menu--active");
-  nav.classList.remove("menu__body--active");
-  body.classList.remove("body--overflow");
-});
-menuBtn.addEventListener("click", showMenu);
